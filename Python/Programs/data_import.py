0b5b337d442173f936ba7fdf0317707f723609a7
jameswawright <jameswawright@icloud.com>
2023-06-14 14:08:05 +0100

    structure program

--- Python/Programs/data_import.py
+++ Python/Programs/data_import.py
@@ -6,12 +6,7 @@
 #             Graduate Programmer                                                                #
 #             Katalyze Data Ltd.                                                                 #
 ##################################################################################################
-#### Import required libraries - temporary
-from pathlib import Path
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 
 
